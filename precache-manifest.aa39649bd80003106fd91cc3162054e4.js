bb71679fb51aee58b1dc2035c0c3e6852d5df2f3
shusnain <sibgatulhusnain@gmail.com>
2019-04-02 01:14:08 -0400

    Updates

---
+++ precache-manifest.aa39649bd80003106fd91cc3162054e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "eea43d91204d65bf480d",
+    "url": "/test-website/static/js/runtime~main.9c24e08d.js"
+  },
+  {
+    "revision": "2c88984a3946f4cb8688",
+    "url": "/test-website/static/js/main.39034a6f.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/test-website/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "2c88984a3946f4cb8688",
+    "url": "/test-website/static/css/main.8032aa5c.chunk.css"
+  },
+  {
+    "revision": "eb109425f26e35df5ec3aea6991ae715",
+    "url": "/test-website/index.html"
+  }
+];
